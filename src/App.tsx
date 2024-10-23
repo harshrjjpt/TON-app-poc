c34e8b0148766f9f21f228d3bf3ee7759fc0cf57
Harsh Rajput <harshrjjpt@Harshs-MacBook-Pro.local>
2024-10-23 14:20:05 +0530

    pushed changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '@twa-dev/sdk';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-       
+      <div className='content'>
+      <img src="https://d2yxqfr8upg55w.cloudfront.net/assets/svg/updated-dapplooker-logo-with-text.svg" alt="" />
+      <h1><span>Hello! 👋</span> from the TON app POC </h1>
       </div>
-      <h1>Hello from the TON app POC </h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
