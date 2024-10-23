fad8d9a07412c55b113faec0f365d136c09da76d
Harsh Rajput <harshrjjpt@Harshs-MacBook-Pro.local>
2024-10-23 14:04:59 +0530

    updated html

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello from the TON app POC </h1>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
