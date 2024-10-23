3878444dbe062d7675ea4fb8d73af98d79838d31
Harsh Rajput <harshrjjpt@Harshs-MacBook-Pro.local>
2024-10-23 13:49:17 +0530

    added  TWA SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import '@twa-dev/sdk';
 import './App.css'
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
